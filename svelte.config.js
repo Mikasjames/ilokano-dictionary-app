0a30f500fd2cb1bb0662a0fda47ef9f858eba398
Mikas James <mikasjames@gmail.com>
2025-04-05 16:21:18 +0800

    Should I turn this into an SPA instead?:????????

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: "404.html" // Required for SPAs on GitHub Pages
+			fallback: null,
+			pages: "build",
+			assets: "build",
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH || ""
