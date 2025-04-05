786f70f2d230b6525edafa1b660422bca41d723e
Mikas James <mikasjames@gmail.com>
2025-04-06 07:10:36 +0800

    Add transition when looking up words

--- src/lib/components/custom/Definition.svelte
+++ src/lib/components/custom/Definition.svelte
@@ -38,63 +38,65 @@
 	}
 </script>
 
-{#each definitions as def}
-	<Card class="w-full">
-		<CardHeader>
-			<div class="flex justify-between items-center">
-				<div>
-					<CardTitle class="text-3xl font-bold">{word}</CardTitle>
-					<CardDescription
-						>{`${def.conjugation ? def.conjugation + " • " : ""}`}{getPartsOfSpeech(
-							def.part_of_speech
-						)}</CardDescription
-					>
-				</div>
-			</div>
-		</CardHeader>
-
-		<CardContent class="space-y-6">
-			<div>
-				<h3 class="text-lg font-medium mb-2">Definition</h3>
-				<div class="bg-muted/50 p-4 rounded-md">
-					<p class="text-lg">{def.definition}</p>
-				</div>
-			</div>
-
-			{#if def.ilok_example || def.eng_example}
-				<Separator />
-				<div>
-					<h3 class="text-lg font-medium mb-2">Examples</h3>
-					<ul class="list-disc ml-6 space-y-2">
-						<li>{def.ilok_example}</li>
-						<li>{def.eng_example}</li>
-					</ul>
-				</div>
-			{/if}
-
-			{#if def.synonyms}
-				<Separator />
-				<div>
-					<h3 class="text-lg font-medium mb-2">Synonyms</h3>
-					<div class="flex flex-wrap gap-2">
-						{#each def.synonyms as synonym}
-							<Badge class="cursor-pointer">{synonym}</Badge>
-						{/each}
+<div class="space-y-4">
+	{#each definitions as def}
+		<Card class="w-full">
+			<CardHeader>
+				<div class="flex justify-between items-center">
+					<div>
+						<CardTitle class="text-3xl font-bold">{word}</CardTitle>
+						<CardDescription
+							>{`${def.conjugation ? def.conjugation + " • " : ""}`}{getPartsOfSpeech(
+								def.part_of_speech
+							)}</CardDescription
+						>
 					</div>
 				</div>
-			{/if}
+			</CardHeader>
 
-			{#if def.antonyms}
-				<Separator />
+			<CardContent class="space-y-6">
 				<div>
-					<h3 class="text-lg font-medium mb-2">Antonyms</h3>
-					<div class="flex flex-wrap gap-2">
-						{#each def.antonyms as antonym}
-							<Badge class="cursor-pointer">{antonym}</Badge>
-						{/each}
+					<h3 class="text-lg font-medium mb-2">Definition</h3>
+					<div class="bg-muted/50 p-4 rounded-md">
+						<p class="text-lg">{def.definition}</p>
 					</div>
 				</div>
-			{/if}
-		</CardContent>
-	</Card>
-{/each}
+
+				{#if def.ilok_example || def.eng_example}
+					<Separator />
+					<div>
+						<h3 class="text-lg font-medium mb-2">Examples</h3>
+						<ul class="list-disc ml-6 space-y-2">
+							<li>{def.ilok_example}</li>
+							<li>{def.eng_example}</li>
+						</ul>
+					</div>
+				{/if}
+
+				{#if def.synonyms}
+					<Separator />
+					<div>
+						<h3 class="text-lg font-medium mb-2">Synonyms</h3>
+						<div class="flex flex-wrap gap-2">
+							{#each def.synonyms as synonym}
+								<Badge class="cursor-pointer">{synonym}</Badge>
+							{/each}
+						</div>
+					</div>
+				{/if}
+
+				{#if def.antonyms}
+					<Separator />
+					<div>
+						<h3 class="text-lg font-medium mb-2">Antonyms</h3>
+						<div class="flex flex-wrap gap-2">
+							{#each def.antonyms as antonym}
+								<Badge class="cursor-pointer">{antonym}</Badge>
+							{/each}
+						</div>
+					</div>
+				{/if}
+			</CardContent>
+		</Card>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	import Definition from "$lib/components/custom/Definition.svelte";
-	import { slide } from "svelte/transition";
+	import { fly } from "svelte/transition";
 
 	export let data;
-
 	$: ({ word } = data);
+
+	const duration = 400;
 </script>
 
 {#key word}
-	<div transition:slide={{}}>
+	<div
+		in:fly={{
+			delay: duration,
+			x: 20,
+			duration
+		}}
+		out:fly={{
+			x: -20,
+			duration: duration
+		}}
+	>
 		<Definition {word} />
 	</div>
 {/key}
