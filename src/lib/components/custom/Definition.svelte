c688092457ea7b43d465f910ad8b06044aa6b02c
Mikas James <mikasjames@gmail.com>
2025-08-28 23:15:53 +0800

    Refactor Definition component to utilize BadgeWords for displaying common forms, variations, synonyms, and antonyms

--- src/lib/components/custom/Definition.svelte
+++ src/lib/components/custom/Definition.svelte
@@ -7,9 +7,9 @@
 		CardContent
 	} from "$lib/components/ui/card";
 	import { Separator } from "$lib/components/ui/separator";
-	import { Badge } from "$lib/components/ui/badge";
 	import { getPartsOfSpeech, processCommasAndDots } from "$lib/utils";
 	import type { Definition } from "$lib/types/types";
+	import BadgeWords from "./BadgeWords.svelte";
 
 	let { word }: { word: string | null | undefined } = $props();
 
@@ -37,80 +37,82 @@
 </script>
 
 <div class="space-y-4">
-	{#each definitions as def}
+	{#if definitions.length === 0}
 		<Card class="w-full">
-			<CardHeader>
-				<div class="flex justify-between items-center">
-					<div>
-						<CardTitle class="text-3xl font-bold">{word}</CardTitle>
-						<CardDescription
-							>{`${def.conjugation ? def.conjugation + " • " : ""}`}{getPartsOfSpeech(
-								def.part_of_speech
-							)}</CardDescription
-						>
-					</div>
-				</div>
-			</CardHeader>
-
-			<CardContent class="space-y-6">
-				<div>
-					<h3 class="text-lg font-medium mb-2">Definition</h3>
-					<div class="bg-muted/50 p-4 rounded-md">
-						<p class="text-lg">{def.definition}</p>
-					</div>
-				</div>
-
-				{#if def.ilok_example || def.eng_example}
-					<Separator />
-					<div>
-						<h3 class="text-lg font-medium mb-2">Examples</h3>
-						<ul class="list-disc ml-6 space-y-2">
-							<li>{def.ilok_example}</li>
-							<li>{def.eng_example}</li>
-						</ul>
-					</div>
-				{/if}
-
-				{#if def.common_forms}
-					<Separator />
-					<div>
-						<h3 class="text-lg font-medium mb-2">Common Forms</h3>
-						<div class="flex flex-wrap gap-2">
-							{#each def.common_forms as cf}
-								<Badge class="cursor-pointer">{cf}</Badge>
-							{/each}
-						</div>
-					</div>
-				{/if}
-
-				{#if def.synonyms}
-					<Separator />
-					<div>
-						<h3 class="text-lg font-medium mb-2">Synonyms</h3>
-						<div class="flex flex-wrap gap-2">
-							{#each def.synonyms as synonym}
-								{#each processCommasAndDots(synonym) as syn}
-									<Badge class="cursor-pointer">{syn}</Badge>
-								{/each}
-							{/each}
-						</div>
-					</div>
-				{/if}
-
-				{#if def.antonyms}
-					<Separator />
-					<div>
-						<h3 class="text-lg font-medium mb-2">Antonyms</h3>
-						<div class="flex flex-wrap gap-2">
-							{#each def.antonyms as antonym}
-								{#each processCommasAndDots(antonym) as ant}
-									<Badge class="cursor-pointer">{ant}</Badge>
-								{/each}
-							{/each}
-						</div>
-					</div>
-				{/if}
+			<CardContent class="text-center">
+				No entry for the word "{word}" found.
 			</CardContent>
 		</Card>
-	{/each}
+	{:else}
+		{#each definitions as def}
+			<Card class="w-full">
+				<CardHeader>
+					<div class="flex justify-between items-center">
+						<div>
+							<CardTitle class="text-3xl font-bold">{word}</CardTitle>
+							<CardDescription
+								>{`${def.conjugation ? def.conjugation + " • " : ""}`}{getPartsOfSpeech(
+									def.part_of_speech
+								)}</CardDescription
+							>
+						</div>
+					</div>
+				</CardHeader>
+
+				<CardContent class="space-y-6">
+					{#if def.definition}
+						<div>
+							<h3 class="text-lg font-medium mb-2">Definition</h3>
+							<div class="bg-muted/50 p-4 rounded-md">
+								<p class="text-lg">{def.definition}</p>
+							</div>
+						</div>
+					{/if}
+
+					{#if def.ilok_example || def.eng_example}
+						<Separator />
+						<div>
+							<h3 class="text-lg font-medium mb-2">Examples</h3>
+							<ul class="list-disc ml-6 space-y-2">
+								<li>{def.ilok_example}</li>
+								<li>{def.eng_example}</li>
+							</ul>
+						</div>
+					{/if}
+
+					{#if def.common_forms}
+						<Separator />
+						<BadgeWords title="Common Forms" wordEntries={def.common_forms} />
+					{/if}
+
+					{#if def.variations}
+						<Separator />
+						<BadgeWords title="Variations" wordEntries={def.variations} />
+					{/if}
+
+					{#if def.synonyms}
+						<Separator />
+						<BadgeWords title="Synonyms"         
+							wordEntries={
+								def.synonyms
+									? def.synonyms.flatMap(synonym => processCommasAndDots(synonym))
+									: []
+							} 
+						/>
+					{/if}
+
+					{#if def.antonyms}
+						<Separator />
+						<BadgeWords title="Antonyms"         
+							wordEntries={
+								def.antonyms
+									? def.antonyms.flatMap(antonym => processCommasAndDots(antonym))
+									: []
+							} 
+						/>
+					{/if}
+				</CardContent>
+			</Card>
+		{/each}
+	{/if}
 </div>
