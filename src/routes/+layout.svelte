d7ef08c38b47a42ccbd436ca14e342b2e5c77a89
Mikasjames <mikasjames@gmail.com>
2026-08-06 12:51:12 +0800

    feat: add Apple-style selection menu with define panel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { Toaster } from "$lib/components/ui/sonner";
 	import { ModeWatcher } from "mode-watcher";
+	import SelectionMenu from "$lib/components/custom/SelectionMenu.svelte";
 	import "../app.css";
 
 	let { children } = $props();
@@ -8,4 +9,5 @@
 
 <ModeWatcher />
 <Toaster position="top-right" />
+<SelectionMenu />
 {@render children()}
